dd78147a95b4eaa373772405abf092c127b5f2f9
George B <705427+georgeblahblah@users.noreply.github.com>
2022-03-26 13:33:46 +0000

    Fetch request token before initiating Pocket flow

--- server.ts
+++ server.ts
@@ -15,8 +15,8 @@ const portWithDefault = (defaultPort: number): number => {
 const port = portWithDefault(8080);
 
 const config = {
-  consumerKey: Deno.env.get("CONSUMER_KEY"),
-  redirectUri: `http://localhost:${port}/requestToken`,
+  consumerKey: Deno.env.get("CONSUMER_KEY") || "",
+  redirectUri: `http://localhost:${port}/callback`,
 };
 
 const log = (text: string): void => console.log(`[pocket-oauth-flow]: ${text}`);
@@ -28,32 +28,40 @@ const htmlResponse = (html: string): Response =>
     },
   });
 
-const root = (): Response => {
-  return htmlResponse(
-    `
-        <html>
-            <body>
-                <p><a href="#" onclick="document.forms[0].submit()">Start</a></p>
-                <form method="post" action="https://getpocket.com/v3/oauth/request">
-                    <input type="hidden" value="${config.consumerKey}" name="consumer_key" />
-                    <input type="hidden" value="${config.redirectUri}" name="redirect_uri" />
-                </form>
-        </html>
-    `
-  );
-};
-
-const handler = (request: Request): Response => {
+const handler = async (request: Request): Promise<Response> => {
   const url = new URL(request.url);
   const path = url.pathname;
   log(`Received request to ${path}`);
   switch (path) {
-    case "/":
-      return root();
+    case "/": {
+      const requestToken = await getRequestToken();
+      const url = new URL(`https://getpocket.com/auth/authorize`);
+      url.searchParams.append("request_token", requestToken);
+      url.searchParams.append("redirect_uri", config.redirectUri);
+      return Response.redirect(url.toString());
+    }
+    case "/callback":
+      return new Response("");
     default:
-      return new Response("Unhandled root");
+      return new Response("Unhandled path");
   }
 };
 
 log(`Webserver running at http://localhost:${port}`);
-await serve(handler, { port });
+serve(handler, { port });
+
+async function getRequestToken(): Promise<string> {
+  const resp = await fetch("https://getpocket.com/v3/oauth/request", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF8",
+      "X-Accept": "application/json",
+    },
+    body: JSON.stringify({
+      consumer_key: config.consumerKey,
+      redirect_uri: config.redirectUri,
+    }),
+  });
+  const json = (await resp.json()) as { code: string };
+  return json.code;
+}
